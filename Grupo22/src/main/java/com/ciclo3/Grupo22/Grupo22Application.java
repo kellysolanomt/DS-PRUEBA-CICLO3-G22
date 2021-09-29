dbb17e25dc4e071f9abae54673f49b7d3e5af165
Kelly Solano <kelly.solano.mt@correo.usa.edu.co>
2021-09-29 18:54:14 -0500

    Creacion de proyecto SpringBoot G22

---
+++ Grupo22/src/main/java/com/ciclo3/Grupo22/Grupo22Application.java
@@ -0,0 +1,13 @@
+package com.ciclo3.Grupo22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo22Application.class, args);
+	}
+
+}
---
+++ Grupo22/src/test/java/com/ciclo3/Grupo22/Grupo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3.Grupo22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
